ec960e6a09613cff9411b1e907f3f6ae5f214843
Hassine Lataoui <hassine.lataoui@intitek.fr>
2022-04-29 15:58:03 +0200

    second commit

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class SecurityController
+ * @package App\Controller
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        /*return $this->render("ui/security/login.html.twig", [
+            "last_username" => $authenticationUtils->getLastUsername(),
+            "error" => $authenticationUtils->getLastAuthenticationError()
+        ]);*/
+        return new Response("hello");
+    }
+
+    /**
+     * @codeCoverageIgnore
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(): void
+    {
+    }
+}
---
+++ tests/PremierTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * Class PremierTest
+ * @package App\Tests
+ */
+class PremierTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+
+        // Request a specific page
+        $crawler = $client->request('GET', '/login');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
+}
